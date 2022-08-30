a9861b1d31c6fecdf3df8f4fbe2467ffa10a46ea
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-30 20:10:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,20 @@
 
 # Created by: @Dev_apollo404
 
-sudo apt-get remove --purge apache2 apache2-utils
-
-
 echo -e "\n\033[1;36mINSTALANDO O APACHE2 \033[1;33mAGUARDE...\033[0m"
-apt-get install apache2 -y > /dev/null 2>&1
-sudo add-apt-repository ppa:ondrej/php
-service apache2 restart > /dev/null 2>&1
 
+
+sudo apt install apache2
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt upgrade
 sudo apt install -y php5.6
 sudo apt update
 sudo apt install php5.6-ssh2
+service apache2 restart
 
-sudo service apache2 restart
+
+
 
 echo -ne "\033[1;32m INFORME A SENHA ROOT\033[1;37m: "; read senha
 cd ../var/www/html

2d27d779b9198a9576a82819ac91c92b6104ae7e
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-31 17:00:36 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ echo -e "\n\033[1;36m####################### \033[1;33mCRIADO POR @Dev_apollo404
 figlet -t -k -w 100 CHECKUSER APOLLO404
 echo -e "\n\033[1;36mINSTALANDO O APACHE2 \033[1;33mAGUARDE...\033[0m"
 
-
 sudo apt install apache2
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
@@ -16,18 +15,12 @@ sudo apt update
 sudo apt install php5.6-ssh2
 service apache2 restart
 
-
-
-
 echo -ne "\033[1;32m INFORME A SENHA ROOT\033[1;37m: "; read senha
 cd ../var/www/html
 echo "<?php \$pass='$senha'?>" > pass.php
-
 curl -o checkuser.php 'https://raw.githubusercontent.com/wellborgmann/checkuserUbuntu18/main/checkuser.php' > /dev/null 2>&1
 cd ../../../etc/apache2
-
 curl -o ports.conf 'https://raw.githubusercontent.com/wellborgmann/checkuserUbuntu18/main/ports.conf' > /dev/null 2>&1
-
 cd sites-available
 curl -o 000-default.conf 'https://raw.githubusercontent.com/wellborgmann/checkuserUbuntu18/main/000-default.conf' > /dev/null 2>&1
 service apache2 restart
